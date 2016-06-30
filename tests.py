199ae5045548006f8fef9a00170b8b3888d21211
not-much-io <kristo.koert@gmail.com>
2016-06-30 14:25:04 +0300

    updated tests

--- tests.py
+++ tests.py
@@ -2,100 +2,93 @@ import unittest
 from character_sheet import *
 
 
-def inc(x: int) -> int:
-    return x + 1
-
-
 class MyTests(unittest.TestCase):
-
-    def test_pipe(self):
-        result = pipe(
-            1,
-            inc,
-            inc,
-            inc
-        )
-        self.assertEqual(result, 4)
-
-    def test_add_level(self):
-        test_data1 = {"experience_points": 0}
-        test_data2 = {"experience_points": 20000}
-        test_data3 = {"experience_points": 300000}
-
-        real_and_expected = (
-            (add_level(test_data1)["level"], 1),
-            (add_level(test_data2)["level"], 6),
-            (add_level(test_data3)["level"], 18)
+    def test_calc_level(self):
+        input_and_expected = (
+            (0, 1),
+            (14000, 6),
+            (265000, 18)
         )
 
-        for real, expected in real_and_expected:
-            self.assertEqual(real, expected)
+        for inp, expected in input_and_expected:
+            self.assertEqual(calc_level(inp), expected)
 
-    def test_add_prof_bonus(self):
-        test_data1 = {"level": 1}
-        test_data2 = {"level": 6}
-        test_data3 = {"level": 18}
-
-        real_and_expected = (
-            (add_prof_bonus(test_data1)["prof_bonus"], 2),
-            (add_prof_bonus(test_data2)["prof_bonus"], 3),
-            (add_prof_bonus(test_data3)["prof_bonus"], 6),
+    def test_calc_prof_bonus(self):
+        input_and_expected = (
+            (1, 2),
+            (6, 3),
+            (18, 6),
         )
 
-        for real, expected in real_and_expected:
-            self.assertEqual(real, expected)
+        for inp, expected in input_and_expected:
+            self.assertEqual(calc_prof_bonus(inp), expected)
 
-    def test_add_ability_modifiers(self):
-        test_data1 = {
-            "abilities": {
-                "str": 12,
-                "dex": 13,
-                "con": 11,
-                "int": 16,
-                "wis": 18,
-                "cha": 13
-            }
+    def test_calc_ability_modifiers(self):
+        input_and_expected = (
+            (AbilityScores(12, 13, 11, 16, 18, 13), AbilityModifiers(1, 1, 0, 3, 4, 1)),
+            (AbilityScores(15, 11, 13, 17, 14, 15), AbilityModifiers(2, 0, 1, 3, 2, 2)),
+        )
+
+        for inp, expected in input_and_expected:
+            self.assertEqual(calc_ability_modifier(inp), expected)
+
+    def test_calc_saving_throws(self):
+        input_and_expected = (
+            (
+                {
+                    "ability_mods": AbilityModifiers(0, 0, 0, 0, 0, 0),
+                    "char_class": bard,
+                    "prof_bonus": 2
+                },
+                SavingThrows(str=0, dex=2, con=0, int=0, wis=0, cha=2)
+            ),
+            (
+                {
+                    "ability_mods": AbilityModifiers(2, 3, 1, -2, -1, 0),
+                    "char_class": monk,
+                    "prof_bonus": 4
+                },
+                SavingThrows(str=6, dex=7, con=1, int=-2, wis=-1, cha=0)
+            )
+        )
+        for inp, expected in input_and_expected:
+            self.assertEqual(calc_saving_throws(**inp), expected)
+
+    def test_calc_skills(self):
+        inp = {
+            "ability_mods": AbilityModifiers(
+                str=0,
+                dex=1,
+                con=2,
+                int=3,
+                wis=4,
+                cha=5),
+            "skill_profs": ("acrobatics", "arcana", "deception"),
+            "prof_bonus": 2
         }
-
-        expected_result1 = dict(
-            test_data1,
-            **{
-                "str_modifier": 1,
-                "dex_modifier": 1,
-                "con_modifier": 0,
-                "int_modifier": 3,
-                "wis_modifier": 4,
-                "cha_modifier": 1
-            }
+        expected = SkillScores(
+            acrobatics=3,
+            animal_handling=4,
+            arcana=5,
+            athletics=0,
+            deception=7,
+            history=3,
+            insight=4,
+            intimidation=5,
+            investigation=3,
+            medicine=4,
+            nature=3,
+            perception=4,
+            performance=5,
+            persuasion=5,
+            religion=3,
+            sleight_of_hand=1,
+            stealth=1,
+            survival=4
         )
 
-        test_data2 = {
-            "abilities": {
-                "str": 15,
-                "dex": 11,
-                "con": 13,
-                "int": 17,
-                "wis": 14,
-                "cha": 15
-            }
-        }
+        self.assertEqual(calc_skills(**inp), expected)
 
-        expected_result2 = dict(
-            test_data2,
-            **{
-                "str_modifier": 2,
-                "dex_modifier": 0,
-                "con_modifier": 1,
-                "int_modifier": 3,
-                "wis_modifier": 2,
-                "cha_modifier": 2
-            }
-        )
 
-        real_and_expected = (
-            (add_ability_modifiers(test_data1), expected_result1),
-            (add_ability_modifiers(test_data2), expected_result2)
-        )
-
-        for real, expected in real_and_expected:
-            self.assertEqual(real, expected)
+if __name__ == '__main__':
+    unittest.main()
